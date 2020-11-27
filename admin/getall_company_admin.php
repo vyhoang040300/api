297b045d41e421b490cf60a0e7af5c6e728cf69b
unknown <minhnh2509it@gmail.com>
2020-11-27 12:25:32 +0700

    admin company

---
+++ admin/getall_company_admin.php
@@ -0,0 +1,37 @@
+<?php
+
+require "../connect.php";
+//
+$query = "SELECT * FROM `company` WHERE 1";
+//
+$data = mysqli_query($conn, $query);
+//
+class Companys{
+	function Companys($id, $name_company, $thumbnail_company, $id_wishlist, $id_sample_email, $email_user, $id_packet){
+		$this->id = $id;
+		$this->nameCompany = $name_company;
+		$this->thumbnailCompany = $thumbnail_company;
+		$this->idWishList = $id_wishlist;
+		$this->idSampleEmail = $id_sample_email;
+		$this->emailUser = $email_user;
+		$this->idPacket = $id_packet;
+	}
+}
+//
+
+$arrayCompanys = array();
+while ($row = mysqli_fetch_assoc($data)) {
+	array_push($arrayCompanys, new Companys(
+		 $row['id']
+		,$row['name_company']
+		,$row['thumbnail_company']
+		,$row['id_wishlist']
+		,$row['id_sample_email']
+		,$row['email_user']
+		,$row['id_packet']));
+
+
+}
+
+echo json_encode($arrayCompanys);
+?>
